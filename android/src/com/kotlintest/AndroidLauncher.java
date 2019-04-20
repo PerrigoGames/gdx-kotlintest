86c769fb6d81b7d68031c49580ff971d783e1edf
Corey Perrigo <corey@perrigogames.com>
2019-04-20 11:15:07 -0700

    Add libgdx project files

---
+++ android/src/com/kotlintest/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.kotlintest;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.kotlintest.KotlinTest;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new KotlinTest(), config);
+	}
+}
---
+++ desktop/src/com/kotlintest/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.kotlintest.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kotlintest.KotlinTest;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new KotlinTest(), config);
+	}
+}
